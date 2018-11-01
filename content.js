1dda40cddb4bf7b033015b88a4632d79835f95dd
Ye Thu <yethu.personal@gmail.com>
2018-11-01 15:26:34 +0630

    Implemented message builder

--- content.js
+++ content.js
@@ -12,28 +12,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   let videoExists = videos.length != 0;
   let currentRate = 0;
 
-  if (!!request) {
-    let { action, data } = request;
-    switch (action) {
-      case ACTIONS.REQUEST_SET_RATE:
-        let { rate } = data;
-        if (videoExists) videos[0].playbackRate = rate;
-        sendResponse({
-          action: ACTIONS.FULFILLED_SET_STATE,
-          success: videoExists,
-          data: { rate },
-        });
-        break;
-      case ACTIONS.REQUEST_QUERY:
-      default:
-        currentRate = videos[0].playbackRate;
-        sendResponse({
-          action: ACTIONS.FULFILLED_QUERY,
-          success: videoExists,
-          data: {
-            rate: currentRate,
-          },
-        });
-    }
+  let { action, data } = request;
+  switch (action) {
+    case ACTIONS.REQUEST_SET_RATE:
+      let { rate } = data;
+      if (videoExists) videos[0].playbackRate = rate;
+      sendResponse(
+        buildMessage(ACTIONS.FULFILLED_SET_RATE, videoExists, { rate })
+      );
+      break;
+    case ACTIONS.REQUEST_QUERY:
+    default:
+      currentRate = videos[0].playbackRate;
+      sendResponse(
+        buildMessage(ACTIONS.FULFILLED_QUERY, videoExists, {
+          rate: currentRate,
+        })
+      );
   }
 });
+
+const buildMessage = (action, success, data) => ({ action, success, data });
